4740b756528665503bc2d49bc3e78350d0ab4f7e
Fidel Ernesto <fidehlg89@gmail.com>
2021-09-23 14:33:10 -0500

    Jquery counter increment and decrement

--- assets/js/code.js
+++ assets/js/code.js
@@ -1,30 +1,99 @@
 $(document).ready(function () {
+    //Adults counter, increment and decrement
+    const adult_minus = $('#quantity__minus-adult');
+    const adult_plus = $('#quantity__plus-adult');
+    const adult_input = $('#quantity__input-adult');
+    adult_minus.click(function (e) {
+        e.preventDefault();
+        var value = adult_input.val();
+        if (value > 1) {
+            value--;
+        }
+        adult_input.val(value);
+    });
+
+    adult_plus.click(function (e) {
+        e.preventDefault();
+        var value = adult_input.val();
+        value++;
+        adult_input.val(value);
+    })
+
+    //Youth counter, increment and decrement
+    const youth_minus = $('#quantity__minus-youth');
+    const youth_plus = $('#quantity__plus-youth');
+    const youth_input = $('#quantity__input-youth');
+    youth_minus.click(function (e) {
+        e.preventDefault();
+        var value = youth_input.val();
+        if (value > 0) {
+            value--;
+        }
+        youth_input.val(value);
+    });
+
+    youth_plus.click(function (e) {
+        e.preventDefault();
+        var value = youth_input.val();
+        value++;
+        youth_input.val(value);
+    })
+
+    //Childrens counter, increment and decrement
+    const childrens_minus = $('#quantity__minus-childrens');
+    const childrens_plus = $('#quantity__plus-childrens');
+    const childrens_input = $('#quantity__input-childrens');
+    childrens_minus.click(function (e) {
+        e.preventDefault();
+        var value = childrens_input.val();
+        if (value > 0) {
+            value--;
+        }
+        childrens_input.val(value);
+    });
+
+    childrens_plus.click(function (e) {
+        e.preventDefault();
+        var value = childrens_input.val();
+        value++;
+        childrens_input.val(value);
+    })
+
     //Prevents to close the specified dropdowns
-    $('#dropdown-menu-2, #dropdown-menu-3').on('click', function (e) {
+    $('#dropdown-menu-2').on('click', function (e) {
+        e.stopPropagation();
+    });
+    $('#dropdown-menu-3').on('click', function (e) {
         e.stopPropagation();
     });
 
+    //Show dropdown and close other
     $('#btn-select-2').on('click', function (e) {
-        $("#btn-select-2").attr("aria-expanded", "true");
-        $("#dropdown-menu-2").show();
+        $("#dropdown-menu-2").addClass("show");
+        $("#dropdown-menu-3").removeClass("show");
     });
+    //Hide dropdown menu
+    $('#btn-done-2').on('click', function (e) {
+        $("#dropdown-menu-2").removeClass("show");
+    });
+
+    //Show dropdown and close other
     $('#btn-select-3').on('click', function (e) {
-        $("#btn-select-3").attr("aria-expanded", "true");
-        $("#dropdown-menu-3").show();
+        $("#dropdown-menu-3").addClass("show");
+        $("#dropdown-menu-2").removeClass("show");
+    });
+    //Hide dropdown menu
+    $('#btn-done-3').on('click', function (e) {
+        $("#dropdown-menu-3").removeClass("show");
     });
 
-    function toggleMenu2() {
-        var $mobileMenu = $('#dropdown-menu-2');
-        $mobileMenu.slideToggle('fast');
+    $('#dropdown-menu-1 #1').on('click', function (e) {
+        var text=$('#dropdown-menu-1 #1').text();
+        $('#btn-select-1 .selected').text(text);
+    });
 
-    }
-    function toggleMenu3() {
-        var $mobileMenu = $('#dropdown-menu-3');
-        $mobileMenu.slideToggle('fast');
-    }
-
-    $('#btn-done-1').on('click', toggleMenu2);
-    $('#btn-done-2').on('click', toggleMenu3);
-
-
+    $('#dropdown-menu-1 #2').on('click', function (e) {
+        var text=$('#dropdown-menu-1 #2').text();
+        $('#btn-select-1 .selected').text(text);
+    });
 });
